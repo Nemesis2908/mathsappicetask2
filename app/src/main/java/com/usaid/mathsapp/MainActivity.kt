e7ee376eb96e0dc277b811e5d7f5d9ad618f4d9e
lab_services_student <Musaid7860@gmail.com>
2025-04-16 13:34:06 +0200

    ice task 3

--- app/src/main/java/com/usaid/mathsapp/MainActivity.kt
+++ app/src/main/java/com/usaid/mathsapp/MainActivity.kt
@@ -1,15 +1,18 @@
 package com.usaid.mathsapp
 
+import android.annotation.SuppressLint
 import android.os.Bundle
 import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
+    @SuppressLint("SetTextI18n")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -31,12 +34,18 @@ class MainActivity : AppCompatActivity() {
             val inputOneInt: Int
             val inputTwoInt: Int
 
-            //convert into number
-            inputOneInt = inputOneString.toInt()
-            inputTwoInt = inputTwoString.toInt()
+            //if(condition){true} else if(2nd condition) {true} else {false}
+            //check if user input is valid
+            if (inputOneString.equals(String) || inputTwoString.equals(String)) {
+            }else tvAnswer.text = "Please enter a number"
 
-           var Answer = inputOneInt + inputTwoInt
-            tvAnswer.text = Answer.toString()
+                //convert into number
+                inputOneInt = inputOneString.toInt()
+                inputTwoInt = inputTwoString.toInt()
+
+                var Answer = inputOneInt + inputTwoInt
+                tvAnswer.text = Answer.toString()
+            
         }//end of equal.setOnClickListener
     }//end of onCreate
 }
